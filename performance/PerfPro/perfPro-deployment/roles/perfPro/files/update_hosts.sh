11b3ea2f8a5dac7228fc573d92b387d6cc2a825d
Rahul Kumar <rahul.kumar@seagate.com>
2020-09-30 06:11:32 -0600

    EOS-13732: changed in server group naming convention for ansible host file, Updated scripts as per requirements

--- performance/PerfPro/perfPro-deployment/roles/perfPro/files/update_hosts.sh
+++ performance/PerfPro/perfPro-deployment/roles/perfPro/files/update_hosts.sh
@@ -1,11 +1,23 @@
 #! /bin/bash
 
 # ./update_hosts client1 server1 server2
-
+File=/etc/ansible/hosts
 yes | cp hosts.bak /etc/ansible/hosts >/dev/null 2>&1
-sed -i "/^\[clientserver\]/a serverclient ansible_host=$1 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
-sed -i "/^\[s3server\]/a serverprimary ansible_host=$2 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
-sed -i "/^\[s3server\]/a serversecondary ansible_host=$3 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
-sed -i "/^\[allserver\]/a serverclient ansible_host=$1 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
-sed -i "/^\[allserver\]/a serverprimary ansible_host=$2 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
-sed -i "/^\[allserver\]/a serversecondary ansible_host=$3 ansible_connection=ssh ansible_user=root" /etc/ansible/hosts
+#if grep -q perfproclientserver "$File"; then
+#  sed -i '/\[perfproclientserver\]/,+11d' $File
+#else
+#  echo -e "\n[perfproclientserver]" > $File 
+#fi
+#if ! grep -q perfpros3server "$File"; then
+#  echo -e "\n[perfpros3server]" >> $File 
+#fi
+#if ! grep -q perfproallserver "$File"; then
+#  echo -e "\n[perfproallserver]" >> $File 
+#fi
+
+sed -i "/^\[perfproclientserver\]/a serverclient ansible_host=$1 ansible_connection=ssh ansible_user=root" $File
+sed -i "/^\[perfpros3server\]/a serverprimary ansible_host=$2 ansible_connection=ssh ansible_user=root" $File
+sed -i "/^\[perfpros3server\]/a serversecondary ansible_host=$3 ansible_connection=ssh ansible_user=root" $File
+sed -i "/^\[perfproallserver\]/a serverclient ansible_host=$1 ansible_connection=ssh ansible_user=root" $File
+sed -i "/^\[perfproallserver\]/a serverprimary ansible_host=$2 ansible_connection=ssh ansible_user=root" $File
+sed -i "/^\[perfproallserver\]/a serversecondary ansible_host=$3 ansible_connection=ssh ansible_user=root" $File

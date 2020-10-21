ed694ef325d0f5d39c3721835e8e237b7e65b04c
Rahul Kumar <rahul.kumar@seagate.com>
2020-10-21 04:54:53 -0600

    [ EOS-13655 ]: PerfPro: Modularization and optimization of code, also updated deployment script

--- performance/PerfPro/perfPro-deployment/roles/perfPro/files/powerCycle.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ -z $1 ]
-then
-        echo "add BMC IP address. usage ./reimage.sh <BMC IP address>"
-else
-        ipmitool -I lanplus -H $1 -U admin -P admin! chassis bootdev pxe
-        ipmitool -I lanplus -H $1 -U admin -P admin! chassis power cycle
-fi
-
--- performance/PerfPro/perfPro-deployment/roles/perfpro_deployment/files/update_hosts.sh
+++ performance/PerfPro/perfPro-deployment/roles/perfpro_deployment/files/update_hosts.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
+# ./update_hosts server1 server2
 yes | cp inventories/perfpro_deployment/hosts.bak inventories/perfpro_deployment/hosts
 sed -i "s/<SRVNODE-1_HOST>/$1/g" inventories/perfpro_deployment/hosts
 sed -i "s/<SRVNODE-2_HOST>/$2/g" inventories/perfpro_deployment/hosts

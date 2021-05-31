3d710bf675f16151aee4c4b81b1d4edcf459b441
Pradeep Kumbhre <pradeep.kumbhre@seagate.com>
2021-05-31 06:23:36 -0600

    EOS-20681: Updated storing 'configuration' for 'custom' field
    Signed-off-by: Pradeep Kumbhre <pradeep.kumbhre@seagate.com>

--- performance/PerfPro/perfPro-deployment/roles/benchmark/files/PerfProBenchmark/hsbench/hsbench_DBupdate.py
+++ performance/PerfPro/perfPro-deployment/roles/benchmark/files/PerfProBenchmark/hsbench/hsbench_DBupdate.py
@@ -108,6 +108,7 @@ def getconfig():
     clients_list=configs_config.get('CLIENTS')
     pc_full=configs_config.get('PC_FULL')
     overwrite=configs_config.get('OVERWRITE')
+    custom=configs_config.get('CUSTOM')
     iteration=configs_config.get('ITERATION')
     cluster_pass=configs_config.get('CLUSTER_PASS')
     change_pass=configs_config.get('CHANGE_PASS')
@@ -120,7 +121,7 @@ def getconfig():
     nfs_mp=configs_config.get('NFS_MOUNT_POINT')
     nfs_fol=configs_config.get('NFS_FOLDER')
 
-    dic={'NODES' :str(nodes_list) , 'CLIENTS' : str(clients_list) ,'BUILD_URL': build_url ,'CLUSTER_PASS': cluster_pass ,'CHANGE_PASS': change_pass ,'PRVSNR_CLI_REPO': prv_cli ,'PREREQ_URL': prereq_url ,'SERVICE_USER': srv_usr ,'SERVICE_PASS': srv_pass , 'PC_FULL': pc_full , 'OVERWRITE':overwrite , 'ITERATION': iteration,'NFS_SERVER': nfs_serv ,'NFS_EXPORT' : nfs_exp ,'NFS_MOUNT_POINT' : nfs_mp , 'NFS_FOLDER' : nfs_fol }
+    dic={'NODES' :str(nodes_list) , 'CLIENTS' : str(clients_list) ,'BUILD_URL': build_url ,'CLUSTER_PASS': cluster_pass ,'CHANGE_PASS': change_pass ,'PRVSNR_CLI_REPO': prv_cli ,'PREREQ_URL': prereq_url ,'SERVICE_USER': srv_usr ,'SERVICE_PASS': srv_pass , 'PC_FULL': pc_full , 'CUSTOM': custom , 'OVERWRITE':overwrite , 'ITERATION': iteration,'NFS_SERVER': nfs_serv ,'NFS_EXPORT' : nfs_exp ,'NFS_MOUNT_POINT' : nfs_mp , 'NFS_FOLDER' : nfs_fol }
     return (dic)
 
 

b9a146502ecdbcc6add98cf07c05eef3cdc6b553
Alexander Sukhachev <alexander.sukhachev@seagate.com>
2021-06-16 09:34:28 -0600

    EOS-20196: improved structure of code
    Deleted outdated code.
    server.py splitted into separate files.
    
    Signed-off-by: Alexander Sukhachev <alexander.sukhachev@seagate.com>

--- performance/PerfLine/roles/perfline_setup/files/webui/views/glances_img.py
+++ performance/PerfLine/roles/perfline_setup/files/webui/views/glances_img.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-#
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -18,26 +16,15 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
+from flask import send_file
 
-import config
-from core import pl_api
 from app_global_data import *
-from views import *
-from views.api import *
-
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 
-@app.after_request
-def add_header(response):
-    # response.cache_control.no_store = True
-    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
-    response.headers['Pragma'] = 'no-cache'
-    response.headers['Expires'] = '-1'
-    return response
-
-
-if __name__ == '__main__':
-    pl_api.init_tq_endpoint("./perfline_proxy.sh")
-    cache.update(config.artifacts_dirs)
-    app.run(**config.server_ep)
+@app.route('/report/<string:tid>/glances_imgs/<string:filename>')
+def serve_glances_imgs(tid, filename):
+    filename_parts = filename.split('__')
+    hostname = filename_parts[0] if len(filename_parts) > 0 else ""
+    location = cache.get_location(tid)
+    path_to_img = f'{location}/result_{tid}/stats/{hostname}/glances/{filename}'
+    return send_file(path_to_img, mimetype='image')

f283f8653443c05872c8952a942a8884515f6a52
Alexander Sukhachev <alexander.sukhachev@seagate.com>
2021-06-16 09:34:28 -0600

    EOS-20196: improved structure of code
    Deleted outdated code.
    server.py splitted into separate files.
    
    Signed-off-by: Alexander Sukhachev <alexander.sukhachev@seagate.com>

---
+++ performance/PerfLine/roles/perfline_setup/files/webui/views/api/log.py
@@ -0,0 +1,61 @@
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+import os
+import json
+import uuid
+import gzip
+from flask import request, make_response
+
+from app_global_data import *
+import config
+
+
+logcookies = {}
+
+
+@app.route('/api/log/<string:morelines>')
+def getlog(morelines: str):
+    cookie = request.cookies.get('logcookie')
+    logf = None
+    uid = None
+
+    # coookie found and active, log is opened
+    if cookie and logcookies.get(cookie):
+        logf = logcookies[cookie]
+    # no coookie found: open log, seek to its end
+    else:
+        uid = str(uuid.uuid1())
+        logf = open(config.logfile, "r")
+        logf.seek(0, os.SEEK_END)
+        logcookies.update({uid: logf})
+
+    # if morelines == "true":  # [sigh]
+    #     logf.seek(0, os.SEEK_END)
+    #     logf.seek(logf.tell() - 2048, os.SEEK_SET)
+
+    data = {
+        "tqlog": "".join(list(logf.readlines()))
+    }
+    content = gzip.compress(json.dumps(data).encode('utf8'), 5)
+    response = make_response(content)
+    response.headers['Content-length'] = len(content)
+    response.headers['Content-Encoding'] = 'gzip'
+    if uid:
+        response.set_cookie('logcookie', uid)
+    return response

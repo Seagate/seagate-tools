b8c263e1a1a6bd584d77ef548b74ad54b55827c0
Rahul Kumar <rahul.kumar@seagate.com>
2021-07-21 10:21:51 +0000

    EOS-22781: Updated variables name from PERFINE_SCRIPT_PATH to PL_SCRIPT_PATH
    Signed-off-by: Rahul Kumar <rahul.kumar@seagate.com>

--- performance/PerfLine/roles/perfline_setup/files/webui/perfline_proxy.sh
+++ performance/PerfLine/roles/perfline_setup/files/webui/perfline_proxy.sh
@@ -3,7 +3,6 @@ SCRIPT_PATH="$(readlink -f $0)"
 SCRIPT_DIR="${SCRIPT_PATH%/*}"
 source "$SCRIPT_DIR/../perfline.conf"
 
-PL_SCRIPT_PATH=$PERFLINE_SCRIPT_PATH
 PL_DIR="${PL_SCRIPT_PATH%/*}"
 
 pushd $PL_DIR > /dev/null

73fc29824415f9229b657454daae7f6d8220a11a
Alexander Sukhachev <alexander.sukhachev@seagate.com>
2021-07-27 07:35:26 -0600

    EOS-22765: removed config updating from worker.sh
    Signed-off-by: Alexander Sukhachev <alexander.sukhachev@seagate.com>

---
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/conf_customization/restore_orig_configs.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+set -e
+set -x
+
+SCRIPT_NAME=`echo $0 | awk -F "/" '{print $NF}'`
+SCRIPT_PATH="$(readlink -f $0)"
+SCRIPT_DIR="${SCRIPT_PATH%/*}"
+
+source "$SCRIPT_DIR/../../../perfline.conf"
+
+EX_SRV="pdsh -S -w $NODES"
+
+HARE_CONF_LOCATION="/var/lib/hare"
+HARE_CONFIG="$HARE_CONF_LOCATION/cluster.yaml"
+HARE_CONFIG_BACKUP="$HARE_CONF_LOCATION/.perfline__cluster.yaml__backup"
+
+function restore_hare_config()
+{
+    $EX_SRV "if [[ -e $HARE_CONFIG_BACKUP ]]; then mv -f $HARE_CONFIG_BACKUP $HARE_CONFIG; fi"
+}
+
+function main()
+{
+    restore_hare_config
+}
+
+main $@
+exit $?
--- performance/PerfLine/roles/perfline_setup/files/wrapper/task_validation/tasks.py
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/task_validation/tasks.py
@@ -56,32 +56,6 @@ def parse_options(conf, result_dir):
        options.append('-s3server_commit_id')
        options.append(conf['custom_build']['s3server_commit_id'])
 
-    if 'configuration' in conf:
-        if 'hare' in conf['configuration']:
-            hare = conf['configuration']['hare']
-            if 'custom_cdf' in hare:
-                options.append('--hare-custom-cdf')
-                options.append(hare['custom_cdf'])
-
-            if 'sns' in hare:
-                options.append('--hare-sns-data-units')
-                options.append(hare['sns']['data_units'])
-
-                options.append('--hare-sns-parity-units')
-                options.append(hare['sns']['parity_units'])
-
-                options.append('--hare-sns-spare-units')
-                options.append(hare['sns']['spare_units'])
-
-            if 'dix' in hare:
-                options.append('--hare-dix-data-units')
-                options.append(hare['dix']['data_units'])
-
-                options.append('--hare-dix-parity-units')
-                options.append(hare['dix']['parity_units'])
-
-                options.append('--hare-dix-spare-units')
-                options.append(hare['dix']['spare_units'])
 
     # Stats collection
     if conf['stats_collection']['iostat']:
@@ -191,6 +165,79 @@ def pack_artifacts(path):
     # rm[f"-rf {path}".split(" ")]()
 
 
+def update_configs(conf, log_dir):
+    options = []
+    result = 'SUCCESS'
+
+    if 'configuration' in conf:
+        if 'hare' in conf['configuration']:
+            hare = conf['configuration']['hare']
+            if 'custom_cdf' in hare:
+                options.append('--hare-custom-cdf')
+                options.append(hare['custom_cdf'])
+
+            if 'sns' in hare:
+                options.append('--hare-sns-data-units')
+                options.append(hare['sns']['data_units'])
+
+                options.append('--hare-sns-parity-units')
+                options.append(hare['sns']['parity_units'])
+
+                options.append('--hare-sns-spare-units')
+                options.append(hare['sns']['spare_units'])
+
+            if 'dix' in hare:
+                options.append('--hare-dix-data-units')
+                options.append(hare['dix']['data_units'])
+
+                options.append('--hare-dix-parity-units')
+                options.append(hare['dix']['parity_units'])
+
+                options.append('--hare-dix-spare-units')
+                options.append(hare['dix']['spare_units'])
+
+        update_configs = plumbum.local["scripts/conf_customization/update_configs.sh"]
+        mv = plumbum.local['mv']
+        try:
+            tee = plumbum.local['tee']
+            (update_configs[options] | tee['/tmp/update_configs.log']) & plumbum.FG
+        except plumbum.commands.processes.ProcessExecutionError:
+            result = 'FAILED'
+
+        mv['/tmp/update_configs.log', log_dir] & plumbum.FG
+
+    return result
+
+
+def restore_original_configs():    
+    result = 'SUCCESS'
+
+    restore_configs = plumbum.local["scripts/conf_customization/restore_orig_configs.sh"]
+    
+    try:
+        (restore_configs['']) & plumbum.FG
+    except plumbum.commands.processes.ProcessExecutionError:
+        result = 'FAILED'
+
+    return result
+
+
+def run_worker(conf, result_dir):
+    result = 'SUCCESS'
+
+    worker = plumbum.local["scripts/worker.sh"]
+    try:
+        tee = plumbum.local['tee']
+        options = parse_options(conf, result_dir)
+        (worker[options] | tee['/tmp/workload.log']) & plumbum.FG
+    except plumbum.commands.processes.ProcessExecutionError:
+        result = 'FAILED'
+
+    mv = plumbum.local['mv']
+    mv['/tmp/workload.log', result_dir] & plumbum.FG
+    return result
+
+
 @huey.task(context=True)
 def worker_task(conf_opt, task):
     conf, opt = conf_opt
@@ -223,19 +270,34 @@ def worker_task(conf_opt, task):
         mkdir = plumbum.local['mkdir']
         mkdir["-p", result["artifacts_dir"]] & plumbum.FG
 
-        run_workload = plumbum.local["scripts/worker.sh"]
-        try:
-            tee = plumbum.local['tee']
-            options = parse_options(conf, result["artifacts_dir"])
-            (run_workload[options] | tee['/tmp/workload.log']) & plumbum.FG
-            result['status'] = 'SUCCESS'
-        except plumbum.commands.processes.ProcessExecutionError:
-            result['status'] = 'FAILED'
+        failed = False
 
-        mv = plumbum.local['mv']
-        mv['/tmp/workload.log', result["artifacts_dir"]] & plumbum.FG
+        if not failed:
+            ret = restore_original_configs()
+            if ret == 'FAILED':
+                result['finish_time'] = str(datetime.now())
+                failed = True
+
+        if not failed:
+            ret = update_configs(conf, result["artifacts_dir"])
+            if ret == 'FAILED':
+                result['finish_time'] = str(datetime.now())
+                failed = True
+
+        if not failed:
+            ret = run_worker(conf, result["artifacts_dir"])
+            if ret == 'FAILED':
+                result['finish_time'] = str(datetime.now())
+                failed = True
+
+        ret = restore_original_configs()
+        if ret == 'FAILED':
+            result['finish_time'] = str(datetime.now())
+            failed = True
+            
 
     result['finish_time'] = str(datetime.now())
+    result['status'] = 'FAILED' if failed else 'SUCCESS'
 
     if 'post_exec_cmds' in conf:
         run_cmds(conf['post_exec_cmds'], result['artifacts_dir'])

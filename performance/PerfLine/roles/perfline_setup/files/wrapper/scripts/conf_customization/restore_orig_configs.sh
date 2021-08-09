35d7bcf6f382ad0c61fa9417fd8f47abfc078efb
Maxim Malezhin <maxim.malezhin@seagate.com>
2021-08-09 16:50:36 +0000

    EOS-22774: Configure LNET and IB (ko2iblnd.ko)
    This patch provides ability to specify custom LNET and IB
    configuration files during submitting of PerfLine workload.
    
    Signed-off-by: Maxim Malezhin <maxim.malezhin@seagate.com>

--- performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/conf_customization/restore_orig_configs.sh
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/conf_customization/restore_orig_configs.sh
@@ -42,6 +42,12 @@ HAPROXY_CONFIG_BACKUP="/etc/haproxy/.perfline__haproxy.cfg__backup"
 S3_CONFIG="/opt/seagate/cortx/s3/conf/s3config.yaml"
 S3_CONFIG_BACKUP="/opt/seagate/cortx/s3/conf/.perfline__s3config.yaml__backup"
 
+LNET_CONFIG="/etc/modprobe.d/lnet.conf"
+LNET_CONFIG_BACKUP="/etc/modprobe.d/.perfline__lnet.conf__backup"
+
+IB_CONFIG="/etc/modprobe.d/ko2iblnd.conf"
+IB_CONFIG_BACKUP="/etc/modprobe.d/.perfline__ko2iblnd.conf__backup"
+
 function restore_hare_config()
 {
     $EX_SRV "if [[ -e $HARE_CONFIG_BACKUP ]]; then mv -f $HARE_CONFIG_BACKUP $HARE_CONFIG; fi"
@@ -62,12 +68,34 @@ function restore_s3_config()
     $EX_SRV "if [[ -e $S3_CONFIG_BACKUP ]]; then mv -f $S3_CONFIG_BACKUP $S3_CONFIG; fi"
 }
 
+function restore_lnet_config()
+{
+    $EX_SRV "if [[ -e $LNET_CONFIG_BACKUP ]]; then mv -f $LNET_CONFIG_BACKUP $LNET_CONFIG; fi"
+}
+
+function restore_ib_config()
+{
+    $EX_SRV "if [[ -e $IB_CONFIG_BACKUP ]]; then mv -f $IB_CONFIG_BACKUP $IB_CONFIG; fi"
+}
+
+function apply_configs()
+{
+    set +e
+    $EX_SRV "systemctl stop lnet"
+    $EX_SRV "systemctl start lnet"
+    $EX_SRV "systemctl restart haproxy"
+    set -e
+}
+
 function main()
 {
     restore_hare_config
     restore_motr_config
     restore_haproxy_config
     restore_s3_config
+    restore_lnet_config
+    restore_ib_config
+    apply_configs
 }
 
 main $@

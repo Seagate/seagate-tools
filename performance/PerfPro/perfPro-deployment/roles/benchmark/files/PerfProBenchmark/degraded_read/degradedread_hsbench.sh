6714f2c86bb3bda64ed922036e12046ea2c4f3cb
Rahul Ranjan <rahul.ranjan@seagate.com>
2021-09-07 06:39:15 -0600

    EOS-23477 , EOS-23475 : implemented DB update and confiuration for Degraded read
    Signed-off-by: Rahul Ranjan <rahul.ranjan@seagate.com>

--- performance/PerfPro/perfPro-deployment/roles/benchmark/files/PerfProBenchmark/degraded_read/degradedread_hsbench.sh
+++ performance/PerfPro/perfPro-deployment/roles/benchmark/files/PerfProBenchmark/degraded_read/degradedread_hsbench.sh
@@ -18,14 +18,15 @@ SIZE_OF_OBJECTS=""
 JSON_FILENAME=				
 OUTPUT_FILE=
 FLAGS=""
+CLUSTER_STATE=""
 TIMESTAMP=`date +'%Y-%m-%d_%H:%M:%S'`
 MAIN="/root/PerfProBenchmark/main.yml"
 CONFIG="/root/PerfProBenchmark/config.yml"
 BUILD=`python3 /root/PerfProBenchmark/read_build.py $CONFIG 2>&1`
-RESULT_DIR=/root/PerfProBenchmark/perfpro_build$BUILD/results
+RESULT_DIR=/root/PerfProBenchmark/perfpro_build$BUILD/results/degraded_read
 validate_args() {
 
-        if [[ -z $NO_OF_BUCKET ]] ||  [[ -z $SIZE_OF_OBJECTS ]] || [[ -z $NO_OF_OBJECTS ]] || [[ -z $NO_OF_THREADS ]] || [[ -f $FLAGS ]];
+        if [[ -z $NO_OF_BUCKET ]] ||  [[ -z $SIZE_OF_OBJECTS ]] || [[ -z $NO_OF_OBJECTS ]] || [[ -z $NO_OF_THREADS ]] || [[ -f $FLAGS ]] || [[ -c $CLUSTER_STATE ]];
         then
                 show_usage
         fi
@@ -33,14 +34,15 @@ validate_args() {
 }
 
 show_usage() {
-        echo -e "\n \t  Usage : ./run_benchmark.sh -b \"NO_OF_BUCKET\"  -o \"NO_OF_OBJECTS\"  -s \"SIZE_OF_OBJECT\" -t \"NO_OF_THREADS\" [ -d TEST_DURATION ] -f FLAGS \n"
+        echo -e "\n \t  Usage : ./run_benchmark.sh -b \"NO_OF_BUCKET\"  -o \"NO_OF_OBJECTS\"  -s \"SIZE_OF_OBJECT\" -t \"NO_OF_THREADS\" [ -d TEST_DURATION ] -f FLAGS -cs CLUSTER_STATE \n"
         echo -e "\t -b\t:\t number of buckets \n"
         echo -e "\t -o\t:\t number of objects [optional] default is '4096'\n"
         echo -e "\t -s\t:\t size of the objects K|M\n"
         echo -e "\t -t\t:\t number of the Threads\n"
         echo -e "\t -d\t:\t TEST_DURATION [optional] default is '600'\n"
         echo -e "\t -f\t:\t FLAGS for HSbench \n"
-        echo -e "\tExample\t:\t ./run_benchmark.sh -b \"8,16,32,64\" -o \"1024,2048\" -s \"1Mb,4Mb,16Mb,32Mb\" -t \"32,48,64,96,128\" -d 600 \n"
+        echo -e "\t -f\t:\t CLUSTER_STATE \n"
+        echo -e "\tExample\t:\t ./run_benchmark.sh -b \"8,16,32,64\" -o \"1024,2048\" -s \"1Mb,4Mb,16Mb,32Mb\" -t \"32,48,64,96,128\" -d 600 -f cxip -cs normal_read \n"
         exit 1
 }
 
@@ -61,7 +63,7 @@ hotsause_benchmark()
                for size in ${OBJ_SIZE//,/ }
                do
                  echo -e "Thread: $client \t SAMPLE: $sample \t OBJECT_SIZE: $size \t BUCKET: $bucket"        
-                 JSON_FILENAME=$TOOL_NAME\_object_$size\_numsamples_$sample\_buckets_$bucket\_sessions_$client\.json
+                 JSON_FILENAME=$TOOL_NAME\_$CLUSTER_STATE\_object_$size\_numsamples_$sample\_buckets_$bucket\_sessions_$client\.json
                  obj_size=$(echo "$size" | tr -d 'b')
                  NUMSAMPLE=$((sample / client))
                  TOOL_DIR=$BENCHMARKLOG/$TOOL_NAME/numclients_$client/buckets_$bucket/$size
@@ -107,6 +109,10 @@ while [ ! -z $1 ]; do
                 FLAGS="$1"
         ;;
 
+        -cs)     shift
+                CLUSTER_STATE="$1"
+        ;;
+
         *)
                 show_usage
                 break
@@ -125,6 +131,7 @@ if [ ! -d $BENCHMARKLOG ]; then
     hotsause_benchmark 2>&1 | tee $BENCHMARKLOG/output.log
     sleep 20 
 #    python3 hsbench_DBupdate.py $BENCHMARKLOG $MAIN $CONFIG 
+    mkdir -p $RESULT_DIR/
     cp -r $BENCHMARKLOG/$TOOL_NAME $RESULT_DIR/    
     cp $BENCHMARKLOG/output.log $RESULT_DIR/$TOOL_NAME/    
       
@@ -134,6 +141,7 @@ else
     hotsause_benchmark 2>&1 | tee $BENCHMARKLOG/output.log
     sleep 20 
 #    python3 hsbench_DBupdate.py $BENCHMARKLOG $MAIN $CONFIG
+    mkdir -p $RESULT_DIR/
     cp -r $BENCHMARKLOG/$TOOL_NAME $RESULT_DIR/    
     cp  $BENCHMARKLOG/output.log $RESULT_DIR/$TOOL_NAME/    
 

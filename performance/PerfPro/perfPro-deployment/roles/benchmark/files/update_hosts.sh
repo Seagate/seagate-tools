1d81422f24468d2e06e891b7c9c6db2b434e3325
Rajesh Deshmukh <rajesh.deshmukh@seagate.com>
2021-10-18 08:05:49 -0600

    EOS-18863 : removed duplicate files
    Signed-off-by: Rajesh Deshmukh <rajesh.deshmukh@seagate.com>

--- performance/PerfPro/perfPro-deployment/roles/benchmark/files/update_hosts.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/bash
-yes | cp inventories/perfpro_deployment/hosts.bak inventories/perfpro_deployment/hosts
-sed -i "s/<SRVNODE-1_HOST>/$1/g" inventories/perfpro_deployment/hosts
-sed -i "s/<SRVNODE-2_HOST>/$2/g" inventories/perfpro_deployment/hosts
-sed -i "s/<CLIENT_HOST>/$3/g" inventories/perfpro_deployment/hosts

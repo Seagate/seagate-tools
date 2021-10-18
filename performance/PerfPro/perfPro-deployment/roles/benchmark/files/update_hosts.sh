7c7f7d6dd8c5dd64aeb54fa045ddbbb92fddb320
Rajesh Deshmukh <rajesh.deshmukh@seagate.com>
2021-10-18 02:58:07 -0600

    EOS-18863 : replacing updated new files
    Signed-off-by: Rajesh Deshmukh <rajesh.deshmukh@seagate.com>

--- performance/PerfPro/perfPro-deployment/roles/benchmark/files/update_hosts.sh
+++ performance/PerfPro/perfPro-deployment/roles/benchmark/files/update_hosts.sh
@@ -3,4 +3,3 @@ yes | cp inventories/perfpro_deployment/hosts.bak inventories/perfpro_deployment
 sed -i "s/<SRVNODE-1_HOST>/$1/g" inventories/perfpro_deployment/hosts
 sed -i "s/<SRVNODE-2_HOST>/$2/g" inventories/perfpro_deployment/hosts
 sed -i "s/<CLIENT_HOST>/$3/g" inventories/perfpro_deployment/hosts
-

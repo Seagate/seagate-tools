c53c3ae426ebe1d268f42ce5d39cd574f79f121a
Tushar Jain <tushar.1.jain@seagate.com>
2021-11-25 19:06:14 -0700

    EOS-26318 Passwordless SSH with multi-key Handling Fix
    Signed-off-by: Tushar Jain <tushar.1.jain@seagate.com>

--- performance/PerfPro/roles/benchmark/files/passwordless_ssh.sh
+++ performance/PerfPro/roles/benchmark/files/passwordless_ssh.sh
@@ -1,6 +1,6 @@
 #!/bin/expect -f
 # ./passwordless_ssh.sh node username pass
-spawn ssh-copy-id -o "StrictHostKeyChecking=no" -o "CheckHostIP=no" [lindex $argv 0]@[lindex $argv 1]
+spawn ssh-copy-id -o "StrictHostKeyChecking=no" -o "CheckHostIP=no" -i /root/.ssh/id_rsa [lindex $argv 0]@[lindex $argv 1]
 set timeout 10
 expect {
     timeout {

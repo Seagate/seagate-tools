4ca491c71a00d9184abed76f8c1ec75795ab937f
arunesh-agrawal <96512519+arunesh-agrawal@users.noreply.github.com>
2022-02-03 18:12:42 +0530

    EOS-28013: Automated Env Setup for Dashboard (#292)
    * EOS-28013: Automated Env Setup for Dashboard
    
    Signed-off-by: Arunesh Agrawal <arunesh.agrawal@seagate.com>
    
    * EOS-28013: Updated README.md
    
    Signed-off-by: Arunesh Agrawal <arunesh.agrawal@seagate.com>

---
+++ performance/Miscellaneous/PerfPro-Dashboard/update_config_ini.py
@@ -0,0 +1,28 @@
+import configparser
+import os
+import sys
+
+cur_dir=os.getcwd()
+req_path="/cortx-test/tools/dash_server/config.ini"
+filepath=cur_dir+req_path
+
+parser = configparser.ConfigParser()
+parser.read(filepath)
+
+t = open(filepath, 'w')
+
+parser.set('PERF_DB', 'db_username', sys.argv[3])
+parser.set('PERF_DB', 'db_password', sys.argv[4])
+
+parser.set('REST', 'db_username', sys.argv[1])
+parser.set('REST', 'db_password', sys.argv[2])
+
+parser.set('MONGODB_URI', 'db_username', sys.argv[1])
+parser.set('MONGODB_URI', 'db_password', sys.argv[2])
+parser.write(t)
+
+t.close()
+
+#print(parser.get('PERF_DB', 'db_password'))
+
+print("*************updated credentials of config.ini****************")

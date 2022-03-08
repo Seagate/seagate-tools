465108f7b4b8c83379125f4a33b6f98f1c9ddc64
Sampada Petkar <sampada.petkar@seagate.com>
2022-03-08 13:39:48 -0700

    CORTX-28017: Implemented Sanity Details UI Page
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>

--- dashboards/superperf/src/App.vue
+++ dashboards/superperf/src/App.vue
@@ -1,26 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <v-app>
+    <v-main>
+      <MainHeader/>
+      <SanityDetails/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainHeader from './components/MainHeader.vue'
+import SanityDetails from './components/SanityDetails.vue'
 
 export default {
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    MainHeader,
+    SanityDetails
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

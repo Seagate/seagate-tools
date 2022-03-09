9bd19283847dfc8b4b3a2b5d0e29266ef06438ef
Sampada Petkar <40736070+sampadap03@users.noreply.github.com>
2022-03-09 11:34:56 +0530

    CORTX-28017: Implement sanity run details UI Page (#383)
    * CORTX-28017: remove old Dashboard code
    
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>
    
    * CORTX-28017: Initial code commit
    
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>
    
    * CORTX-28017: Implemented Sanity Details UI Page
    
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>
    
    * CORTX-28017: Codacy fixes
    
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>

---
+++ dashboards/superperf/src/components/SanityDataTable.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h3>Read {{ TableName }}</h3>
+        <v-row>
+          <v-col>
+            <v-data-table
+              :headers="Headers"
+              :items="DataValuesRead"
+              class="elevation-2"
+              hide-default-footer
+            >
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </v-col>
+
+      <v-col>
+        <h3>Write {{ TableName }}</h3>
+        <v-row>
+          <v-col>
+            <v-data-table
+              :headers="Headers"
+              :items="DataValuesWrite"
+              class="elevation-2"
+              hide-default-footer
+            >
+            </v-data-table>
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    TableName: String,
+    Headers: Array,
+    DataValuesRead: Array,
+    DataValuesWrite: Array,
+  },
+  data() {
+    return {
+      //
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.v-data-table::v-deep th {
+  font-size: 16px !important;
+  background-color: #ced2d6;
+}
+</style>

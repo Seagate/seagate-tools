4bc101874c8c8a4b06ff03f56f2219cd63a2b255
Arunesh Agrawal <arunesh.agrawal@seagate.com>
2022-03-22 05:11:16 -0600

    CORTX-29343: Fixed Critical codacy shell issues
    Signed-off-by: Arunesh Agrawal <arunesh.agrawal@seagate.com>

--- performance/Miscellaneous/PerfPro-Database/run_db_env.sh
+++ performance/Miscellaneous/PerfPro-Database/run_db_env.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
+
 # Fetching mongo-version 
 mongo_version=$(cat db_info | grep ^mongo_version | awk '{print $3}')
 
 # Copying congiguration file
-cp $(pwd)/mongo_release_info /etc/yum.repos.d/mongodb-org-$mongo_version.repo
+cp $(pwd)/mongo_release_info /etc/yum.repos.d/mongodb-org-"$mongo_version".repo
 
 # Updating mongo_version info in configuration file
-sed -i "s/mongo_version/$mongo_version/g" /etc/yum.repos.d/mongodb-org-$mongo_version.repo
+sed -i "s/mongo_version/"$mongo_version"/g" /etc/yum.repos.d/mongodb-org-"$mongo_version".repo
 
 # Installing mongodb
 yum install -y mongodb-org
@@ -28,13 +30,13 @@ path=$(cat db_info | grep ^path | awk '{print $3}')
 db_name=$(cat db_info | grep ^db_name | awk '{print $3}')
 
 # Updating create_user.js
-prev_user=`cat create_user.js |grep ^user: | awk '{print$(NF-1)}'`
-prev_user_pass=`cat create_user.js |grep ^pwd: | awk '{print$(NF-1)}'`
-prev_database=`cat create_user.js |grep db: | awk '{print$(NF-1)}'`
+prev_user=$(cat create_user.js |grep ^user: | awk '{print$(NF-1)}')
+prev_user_pass=$(cat create_user.js |grep ^pwd: | awk '{print$(NF-1)}')
+prev_database=$(cat create_user.js |grep db: | awk '{print$(NF-1)}')
 
-sed -i "s/^user: $prev_user/user: \"$username\"/g" create_user.js
-sed -i "s/^pwd: $prev_user_pass/pwd: \"$password\"/g" create_user.js
-sed -i "s/db: $prev_database/db: \"$db_name\"/g" create_user.js
+sed -i "s/^user: "$prev_user"/user: \"$username\"/g" create_user.js
+sed -i "s/^pwd: "$prev_user_pass"/pwd: \"$password\"/g" create_user.js
+sed -i "s/db: "$prev_database"/db: \"$db_name\"/g" create_user.js
 
 
 # Starting mongo shell
@@ -47,10 +49,10 @@ exit
 elif [ $1 == "dump" ]
 then
 # Dumping the database
-mongodump --host $host --username $username --password $password --port $port -d $db_name -o $path-$(date +%F)
+mongodump --host "$host" --username "$username" --password "$password" --port "$port" -d "$db_name" -o "$path"-$(date +%F)
 
 # Restoring the database
-mongorestore --host localhost --port $port --db $db_name --nsExclude "$db_name.*.json" $path-$(date +%F)/$db_name/
+mongorestore --host localhost --port "$port" --db "$db_name" --nsExclude "$db_name.*.json" "$path"-$(date +%F)/$db_name/
 
 echo "****************************Data is Dumped and Restored**********************************"
 fi

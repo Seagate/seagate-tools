587a2c89b235bc78bab8f83047baef972bfbb308
Rajesh Deshmukh <rajesh.deshmukh@seagate.com>
2022-04-19 05:29:28 -0600

    CORTX-30497 : Moved artifacts to respective directory
    Signed-off-by: Rajesh Deshmukh <rajesh.deshmukh@seagate.com>

--- performance/Miscellaneous/Longevity/afterScript.sh
+++
@@ -1,7 +0,0 @@
-object = 32
-grep -w PutObject s3server-0x7200000000000001\:0x*/s3server.INFO > afterP_$object
-grep -w GetObject s3server-0x7200000000000001\:0x*/s3server.INFO > afterG_$object
-grep -w DeleteObject s3server-0x7200000000000001\:0x*/s3server.INFO > afterD_$object
-grep -w PutObjectTagging s3server-0x7200000000000001\:0x*/s3server.INFO > afterPT_$object
-grep -w GetObjectTagging s3server-0x7200000000000001\:0x*/s3server.INFO > afterGT_$object
-grep -w DeleteObjectTagging s3server-0x7200000000000001\:0x*/s3server.INFO > afterDT_$object

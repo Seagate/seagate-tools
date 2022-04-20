29f4afae62dadec2e3a693ffe0d31d0b0f83669b
Sampada Petkar <sampada.petkar@seagate.com>
2022-04-20 00:56:05 -0600

    CORTX-30496: Migrate AutoPerf to seperate Repository
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>

--- performance/AutoPerf/autoPerf_Dashboard/gui/vue.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  outputDir: "web-dist",
-  devServer: {
-    proxy: "http://localhost:6876"
-  },
-  configureWebpack: {
-    devtool: "source-map"
-  }
-};
--- performance/AutoPerf/autoPerf_Dashboard/web/api/index.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.use('/v1.0', require('./v1.0'));
-
-module.exports = router;

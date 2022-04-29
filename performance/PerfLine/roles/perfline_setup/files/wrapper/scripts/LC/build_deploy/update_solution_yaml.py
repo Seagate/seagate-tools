4fce5968f136748b2cb14215fe00bfe74f9651fa
Alexander Sukhachev <66412421+alexsukhachev@users.noreply.github.com>
2022-04-29 16:53:45 +0300

    CORTX-30440: added 'build and deploy' feature
    Signed-off-by: Alexander Sukhachev <alexander.sukhachev@seagate.com>

---
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/LC/build_deploy/update_solution_yaml.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+#
+#
+# Copyright (c) 2022 Seagate Technology LLC and/or its Affiliates
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+import yaml
+
+
+POD_IMAGETYPE_MAP = {
+    'cortxcontrol': 'cortx-all',
+    'cortxdata': 'cortx-all',
+    'cortxserver': 'cortx-rgw',
+    'cortxha': 'cortx-all',
+    'cortxclient': 'cortx-all'
+    }
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(prog=sys.argv[0],
+                                     formatter_class=argparse.RawDescriptionHelpFormatter,
+                                     description="")
+
+    parser.add_argument("-s", "--src-file", type=str, required=True,
+                        help="path to config file used as base")
+
+    parser.add_argument("-d", "--dst-file", type=str, required=True,
+                        help="path to store result")
+
+    parser.add_argument("--pod", action='append', default=list(), required=False)
+    parser.add_argument("--image", action='append', default=list(), required=False)
+
+    return parser.parse_args()
+
+
+def parse_file(file_path):
+    with open(file_path, 'rt') as f:
+        data = yaml.safe_load(f.read())
+
+    return data
+
+
+def save_file(data, file_path):
+    with open(file_path, 'wt') as f:
+        f.write(yaml.safe_dump(data, sort_keys=False))
+
+
+def find_pods_by_docker_img_type(conf, image_type):
+    pods = list()
+
+    for pod, img_type in POD_IMAGETYPE_MAP.items():
+        if image_type == img_type:
+            pods.append(pod)
+
+    return pods
+
+
+def update_docker_image_type(conf, image_descr):
+    values = image_descr.split('=', maxsplit=1)
+
+    if len(values) != 2:
+        raise ValueError(f'invalid format of --image option: {image_descr}')
+
+    image_type, docker_img = values
+    pods = find_pods_by_docker_img_type(conf, image_type)
+
+    for pod_name in pods:
+        print(f'set {pod_name} to {docker_img}')
+        conf['solution']['images'][pod_name] = docker_img
+
+
+
+def update_pod_value(conf, pod_descr):
+    values = pod_descr.split('=', maxsplit=1)
+
+    if len(values) != 2:
+        raise ValueError(f'invalid format of --pod option: {pod_descr}')
+
+    pod_name, docker_img = values
+    images = conf['solution']['images']
+
+    if pod_name not in images:
+        raise ValueError(f'{pod_name} not found in the solution.yaml file')
+
+    print(f'set {pod_name} to {docker_img}')
+    images[pod_name] = docker_img
+
+
+def main():
+    args = parse_args()
+    data = parse_file(args.src_file)
+
+    for image_descr in args.image:
+        update_docker_image_type(data, image_descr)
+
+    for pod_descr in args.pod:
+        update_pod_value(data, pod_descr)
+
+    save_file(data, args.dst_file)
+
+
+if __name__ == '__main__':
+    main()

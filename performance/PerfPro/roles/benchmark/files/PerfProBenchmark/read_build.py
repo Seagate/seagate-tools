0ab10517850ef255b26de053dbffdd4ae0a726d2
rahulr <rahul.ranjan@seagate.com>
2022-05-13 05:52:32 -0600

    CORTX-30503 : Removed passwords and credentials from PerfPro repo
    Signed-off-by: rahulr <rahul.ranjan@seagate.com>

--- performance/PerfPro/roles/benchmark/files/PerfProBenchmark/read_build.py
+++ performance/PerfPro/roles/benchmark/files/PerfProBenchmark/read_build.py
@@ -1,30 +1,28 @@
+import os
 import sys
 import yaml
-import urllib.request
 import re
-
 
 conf_yaml = open(sys.argv[1])
 parse_conf = yaml.safe_load(conf_yaml)
 release_info=str(parse_conf.get('BUILD_INFO'))
 build_url=parse_conf.get('BUILD_URL')
+docker_info=parse_conf.get('DOCKER_INFO')
 
 def get_build_info(variable):
-
-    if build_url.lower().startswith('http'):
-        release_info = urllib.request.urlopen(build_url+'RELEASE.INFO')
-    else:
-        raise Exception("bad build_url")
-
-    for line in release_info:
-        if variable in line.decode("utf-8"):
-            strinfo=line.decode("utf-8").strip()
+    release_info=os.popen('docker run --rm -it ' + docker_info +' cat /opt/seagate/cortx/RELEASE.INFO')
+    lines=release_info.readlines()
+    for line in lines:
+        if variable in line:
+            strinfo=line.strip()
             strip_strinfo=re.split(': ',strinfo)
             return(strip_strinfo[1])
 
 if release_info == 'RELEASE.INFO':
-    BUILD=get_build_info('BUILD')
-    print(BUILD[1:-1])
+    version=get_build_info('VERSION')[1:-1]
+    ver_strip=re.split('-',version)
+    BUILD=ver_strip[1]
+    print(BUILD)
 elif release_info == 'USER_INPUT':
     BUILD=parse_conf.get('BUILD')
     print(BUILD)

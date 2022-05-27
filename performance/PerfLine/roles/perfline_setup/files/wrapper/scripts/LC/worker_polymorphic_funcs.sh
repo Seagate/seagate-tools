998fcbedc454a6d0208c6015c9de1756ca605b0b
Maxim Malezhin <maxim.malezhin@seagate.com>
2022-05-27 05:57:34 -0600

    Fixed IP addr obtating in case of POD restart
    Signed-off-by: Maxim Malezhin <maxim.malezhin@seagate.com>

--- performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/LC/worker_polymorphic_funcs.sh
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/scripts/LC/worker_polymorphic_funcs.sh
@@ -257,7 +257,7 @@ function prepare_cluster() {
     ssh "$PRIMARY_NODE" 'sed -i "/seagate/d" /etc/hosts'
     for node in ${NODES//,/ };
     do
-        local ip=$(ssh "$PRIMARY_NODE" "kubectl get pod -o wide | grep $node | grep $CORTX_SERVER_POD_PREFIX" | awk '{print $6}')
+        local ip=$(ssh "$PRIMARY_NODE" "kubectl get pods -o custom-columns=NAME:.metadata.name,NODE:.spec.nodeName,IP:.status.podIP | grep $node | grep $CORTX_SERVER_POD_PREFIX" | awk '{print $3}')
         ((i=i+1))
         if [ -z "$ip" ]; then
            echo "s3 ip detection failed"

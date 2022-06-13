f2ff81c62a6e2d5034098f926cf1631b57299cf9
Rahul Kumar <rahul.kumar@seagate.com>
2022-06-13 00:13:21 -0600

    CORTX-30475: Adding support for new service framework v0.7.0
    Signed-off-by: Rahul Kumar <rahul.kumar@seagate.com>

--- performance/PerfLine/roles/perfline_setup/files/wrapper/stat/extract_disks.py
+++ performance/PerfLine/roles/perfline_setup/files/wrapper/stat/extract_disks.py
@@ -29,56 +29,33 @@ import yaml
 import re
 
 cluster_config_file = sys.argv[1]
-assigned_IPs = sys.argv[2:]
-hosts_file_path = '/etc/hosts'
 
-def check_IPs(hosts_file_path): # function to check IPs present for all nodes
-    host_file_lines = open(hosts_file_path).readlines()
-
-    srv_node_pattern = r'srvnode-(?P<node_id>[0-9]+)'
-
-    ips_map = {}
-    for line in host_file_lines:
-        if not line.strip().startswith("#"):
-           match_res = re.search(srv_node_pattern, line)
-           if match_res:
-               try:
-                   ips_map['srvnode-{}'.format(match_res.group('node_id'))] = line.split()[0]
-               except Exception as e:
-                   print(f"Error: {e}")
-    return ips_map
-
-def get_disks(ips_map, ips): # returns disks from cluster config file
-    for node in range(1, len(ips_map)+1):
-        ip = ips_map['srvnode-{}'.format(node)]
+def get_disks(): # returns disks from cluster config file
         try:
-            if ip and ip in ips: # checks for ips in assigned ips
-                with open(sys.argv[1], 'r') as config_file:
-                    config = yaml.safe_load(config_file)
+           with open(sys.argv[1], 'r') as config_file:
+               config = yaml.safe_load(config_file)
 
-                disks = []
-                md_disks = []
+           disks = []
+           md_disks = []
 
-                for m0_server in config['nodes'][node-1]['m0_servers']:
-                    disks.extend(m0_server['io_disks']['data'])
+           for machine in config['cluster']['storage_set'][0]['nodes']:
+               if "cvg" in config['node'][machine]:
+                   for m0_server in config['node'][machine]['cvg']:
+                       disks.extend(m0_server['devices']['data'])
+                       if 'metadata' in m0_server['devices']:
+                           md_disk = m0_server['devices']['metadata']
+                           if md_disk:
+                               md_disks.extend(md_disk)
 
-                    if 'meta_data' in m0_server['io_disks']:
-                        md_disk = m0_server['io_disks']['meta_data']
-                        if md_disk:
-                            md_disks.append(md_disk)
-
-                return disks, md_disks, node
+           return disks, md_disks
         except Exception as e:
             print(f"Error: {e}")
-    return None # returns none if not found
-
 
 def main():
-    ips_map = check_IPs(hosts_file_path)
-    disks, md_disks, node = get_disks(ips_map, assigned_IPs)
+    disks, md_disks = get_disks()
     if disks:
-        print(node)
         print('IO:' + ' '.join(disks))
         print('MD:' + ' '.join(md_disks))
+
 if __name__ == "__main__":
     main()

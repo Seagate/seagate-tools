916f0b41c992da9c59d893fe33bbe5fbc89bc955
rahulr <rahul.ranjan@seagate.com>
2022-06-27 10:32:42 -0600

    CORTX-32471 : Added doc strings for all the python files
    Signed-off-by: rahulr <rahul.ranjan@seagate.com>
    Signed-off-by: Sampada Petkar <sampada.petkar@seagate.com>

--- performance/PerfPro/roles/benchmark/files/PerfProBenchmark/degraded_read/installHSbench.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [ ! -f "/root/go/bin/hsbench" ] ; then
-   echo "Installing hsbench tools"
-   cd ~; go get github.com/markhpc/hsbench
-   cd /root/go/src/github.com/markhpc/hsbench; go build
-else
-   echo "HSbenchmark Tools is already configured"
-fi

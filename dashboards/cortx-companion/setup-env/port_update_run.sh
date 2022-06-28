e075204ed88dc48fda2e8140fdafba60c73833a8
Arunesh Agrawal <arunesh.agrawal@seagate.com>
2022-06-27 22:06:24 -0600

    CORTX-32375: Updated and tested setup-env scripts
    Signed-off-by: Arunesh Agrawal <arunesh.agrawal@seagate.com>

--- dashboards/cortx-companion/setup-env/port_update_run.sh
+++ dashboards/cortx-companion/setup-env/port_update_run.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 source dashboards/venv/bin/activate
-# echo "$VIRTUAL_ENV"
-
 
 CFT_DB_USERNAME=$(grep CFT_DB_USER_NAME secret | awk '{print$3}')
 CFT_DB_USERPASS=$(grep CFT_DB_USER_PASS secret | awk '{print$3}')
@@ -13,36 +11,34 @@ PERF_DB_USERPASS=$(grep PERF_DB_USER_PASS secret | awk '{print$3}')
 GIT_USER=$(grep GIT_USER_NAME secret | awk '{print$3}')
 GIT_PASS=$(grep GIT_USER_TOKEN secret | awk '{print$3}')
 
+JIRA_USERNAME=$(grep JIRA_USER_NAME secret | awk '{print$3}')
+JIRA_USERPASS=$(grep JIRA_USER_PASS secret | awk '{print$3}')
 
 session_name=$(tmux display-message -p '#S')
-# echo "$session_name"
-
 
 if [ "$session_name" == 'dashboard-main' ]
 then
 echo "working in 'dashboard-main' enviornment'"
 cd dashboards/main/
 
-# Accessing cortx-test repository
+# Accessing seagate-tools repository
 if [ -z "$GIT_USER"  ]
 then
-# open-source cloning
-git clone --recursive https://github.com/Seagate/cortx-test.git -b dev
+# Open-Source cloning
+git clone --recursive https://github.com/Seagate/seagate-tools.git -b main
 else
 # Private repository cloning
-git clone --recursive https://"$GIT_USER":"$GIT_PASS"@github.com/Seagate/cortx-test.git -b dev
+git clone --recursive https://"$GIT_USER":"$GIT_PASS"@github.com/Seagate/seagate-tools.git -b main
 fi
 
 
-# updating username and passswords of configs.yml
-# <file_path: cortx-test/tools/dash_server/Performance/configs.yml>
+# Updating username and passswords of configs.yml
 python3.7 ../../update_config_yml.py "$PERF_DB_USERNAME" "$PERF_DB_USERPASS"
 
-# updating username and passswords of configs.ini
-# <file_path: cortx-test/tools/dash_server/config.ini>
-python3.7 ../../update_config_ini.py "$CFT_DB_USERNAME" "$CFT_DB_USERPASS" "$PERF_DB_USERNAME" "$PERF_DB_USERPASS"
+# Updating username and passswords of configs.ini
+python3.7 ../../update_config_ini.py "$CFT_DB_USERNAME" "$CFT_DB_USERPASS" "$PERF_DB_USERNAME" "$PERF_DB_USERPASS" "$JIRA_USERNAME" "$JIRA_USERPASS"
 
-cd cortx-test/tools/dash_server/
+cd seagate-tools/dashboards/cortx-companion/
 sed -i 's/port=5002/port=5012/g' main_app.py
 python3.7 main_app.py
 
@@ -52,26 +48,24 @@ echo "working in 'dashboard-prod' enviornment'"
 
 cd dashboards/prod/
 
-# Accessing cortx-test repository
+# Accessing seagate-tools repository
 if [ -z "$GIT_USER"  ]
 then
-# open-source cloning
-git clone --recursive https://github.com/Seagate/cortx-test.git -b dev
+# Open-Source cloning
+git clone --recursive https://github.com/Seagate/seagate-tools.git -b main
 else
 # Private repository cloning
-git clone --recursive https://"$GIT_USER":"$GIT_PASS"@github.com/Seagate/cortx-test.git -b dev
+git clone --recursive https://"$GIT_USER":"$GIT_PASS"@github.com/Seagate/seagate-tools.git -b main
 fi
 
 
-# updating username and passswords of configs.yml
-# <file_path: cortx-test/tools/dash_server/Performance/configs.yml>
+# Updating username and passswords of configs.yml
 python3.7 ../../update_config_yml.py "$PERF_DB_USERNAME" "$PERF_DB_USERPASS"
 
-# updating username and passswords of configs.ini
-# <file_path: cortx-test/tools/dash_server/config.ini>
-python3.7 ../../update_config_ini.py "$CFT_DB_USERNAME" "$CFT_DB_USERPASS" "$PERF_DB_USERNAME" "$PERF_DB_USERPASS"
+# Updating username and passswords of configs.ini
+python3.7 ../../update_config_ini.py "$CFT_DB_USERNAME" "$CFT_DB_USERPASS" "$PERF_DB_USERNAME" "$PERF_DB_USERPASS" "$JIRA_USERNAME" "$JIRA_USERPASS"
 
-cd cortx-test/tools/dash_server/
+cd seagate-tools/dashboards/cortx-companion/
 sed -i 's/port=5002/port=5022/g' main_app.py
 sed -i 's/debug=True/debug=False/g' main_app.py
 python3.7 main_app.py
